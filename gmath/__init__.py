71cd25d5c240b502c3c52566401e39ef18e292c2
Dmitry Yushko <yushko07@gmail.com>
2019-09-09 11:31:32 +0300

    Adding 'gmath' in project

---
+++ gmath/__init__.py
@@ -0,0 +1,14 @@
+from .matrix import *
+from .vector import *
+
+
+__all__ = [
+    'vec3',
+    'vec4',
+    'identity_matrix',
+    'translate_matrix',
+    'scale_matrix',
+    'x_rotation_matrix',
+    'y_rotation_matrix',
+    'z_rotation_matrix',
+]
