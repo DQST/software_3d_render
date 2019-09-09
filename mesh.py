bf5703f8bb0b748dc6c22fc12dcf5fdef214659b
Dmitry Yushko <yushko07@gmail.com>
2019-09-09 11:59:41 +0300

    Update mesh.py, refactor and rewrite code.

--- mesh.py
+++ mesh.py
@@ -1,30 +1,45 @@
+import pathlib
 from dataclasses import dataclass
-from typing import Iterable, Optional
+from typing import Optional, List
 
-import numpy as np
-from pyrr import Vector3
+from gmath import vec3
+from help import group_by
+
+
+@dataclass
+class Face:
+    p0: vec3
+    p1: vec3
+    p2: vec3
+
+    @property
+    def normal(self) -> vec3:
+        normal = (self.p1 - self.p0).cross(self.p2 - self.p0)
+        return normal.normalize()
 
 
 @dataclass
 class Mesh:
+    """Mesh class for models"""
     name: str
-    vertexes: Iterable[Vector3]
-    indexes: Iterable[int]
-    position: Vector3 = Vector3([0., 0., 0.])
-    rotation: Vector3 = Vector3([0., 0., 0.])
-    scale: Vector3 = Vector3([1., 1., 1.])
+    vertexes: List[vec3]
+    indexes: List[int]
+    position: vec3 = vec3(0., 0., 0.)
+    rotation: vec3 = vec3(0., 0., 0.)
+    scale: vec3 = vec3(1., 1., 1.)
 
     @property
-    def triangles(self):
-        r = []
-        for i, e in enumerate(self.indexes, 1):
-            r.append(self.vertexes[e-1])
-            if i % 3 == 0:
-                yield r
-                r = []
+    def faces(self):
+        for p0, p1, p2 in group_by(self.indexes, 3):
+            yield Face(self.vertexes[p0 - 1], self.vertexes[p1 - 1], self.vertexes[p2 - 1])
 
     @classmethod
-    def load_from_obj_file(cls, path: str, name: Optional[str] = None):
+    def from_obj_file(cls, path: str, name: Optional[str] = None) -> 'Mesh':
+        """Create Mesh instance from obj file."""
+        path_lib = pathlib.Path(path)
+        if not path_lib.exists():
+            raise ValueError(f'File "{path_lib.name}" not exists.')
+
         vertexes = []
         indexes = []
         with open(path) as file:
@@ -33,7 +48,7 @@ class Mesh:
                     continue
                 name, *args = line.split()
                 if name == 'v':
-                    vertexes.append(Vector3(args, dtype=np.float64))
+                    vertexes.append(vec3(*args))
                 elif name == 'f':
                     indexes.extend(int(i.split('/')[0]) for i in args)
         return cls(name or file.name, vertexes, indexes)
