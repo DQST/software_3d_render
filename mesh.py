9da76a36319dd630e4aec440f637435802668c5b
Dmitry Yushko <yushko07@gmail.com>
2019-09-18 16:32:00 +0300

    Update mesh.py

--- mesh.py
+++ mesh.py
@@ -1,38 +1,38 @@
 import pathlib
-from dataclasses import dataclass
-from typing import Optional, List
+from dataclasses import dataclass, field
+from typing import Optional, List, Iterator
 
-from gmath import vec3
+import glm
 from help import group_by
 
 
 @dataclass
 class Face:
     """Face class"""
-    p0: vec3
-    p1: vec3
-    p2: vec3
+    p0: glm.vec3
+    p1: glm.vec3
+    p2: glm.vec3
+    normal: glm.vec3 = field(init=False, default=None)
 
-    @property
-    def normal(self) -> vec3:
-        normal = (self.p1 - self.p0).cross(self.p2 - self.p0)
-        return normal.normalize()
+    def __post_init__(self):
+        self.normal = glm.normalize(glm.cross(self.p1 - self.p0, self.p2 - self.p0))
 
 
 @dataclass
 class Mesh:
     """Mesh class for models"""
     name: str
-    vertexes: List[vec3]
+    vertexes: List[glm.vec3]
     indexes: List[int]
-    position: vec3 = vec3(0., 0., 0.)
-    rotation: vec3 = vec3(0., 0., 0.)
-    scale: vec3 = vec3(1., 1., 1.)
+    position: glm.vec3 = glm.vec3(0., 0., 0.)
+    rotation: glm.vec3 = glm.vec3(0., 0., 0.)
+    scale: glm.vec3 = glm.vec3(1., 1., 1.)
+    color: glm.vec3 = glm.vec3(1.0, 0.5, 0.31)
 
     @property
-    def faces(self) -> Face:
+    def faces(self) -> Iterator[Face]:
         for p0, p1, p2 in group_by(self.indexes, 3):
-            yield Face(self.vertexes[p0 - 1], self.vertexes[p1 - 1], self.vertexes[p2 - 1])
+            yield Face(self.vertexes[p0], self.vertexes[p1], self.vertexes[p2])
 
     @classmethod
     def from_obj_file(cls, path: str, name: Optional[str] = None) -> 'Mesh':
@@ -49,7 +49,8 @@ class Mesh:
                     continue
                 name, *args = line.split()
                 if name == 'v':
-                    vertexes.append(vec3(*args))
+                    args = [float(i) for i in args]
+                    vertexes.append(glm.vec3(*args))
                 elif name == 'f':
-                    indexes.extend(int(i.split('/')[0]) for i in args)
+                    indexes.extend(int(i.split('/')[0]) - 1 for i in args)
         return cls(name or file.name, vertexes, indexes)
