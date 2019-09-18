46db052e2d66841875084385b8b313d168d00642
Dmitry Yushko <yushko07@gmail.com>
2019-09-18 16:32:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,12 @@ from dataclasses import dataclass, field
 from tkinter import *
 from typing import List
 
-from PIL import Image, ImageTk, ImageDraw
-from pyrr import Matrix44, Vector3
+from PIL import Image, ImageTk
+import glm
 
 from mesh import Mesh
+from pipeline import pipeline, Camera, Light
+
 
 WIDTH = 1024
 HEIGHT = 768
@@ -25,8 +27,7 @@ class Display:
         self._window.geometry(f'{self.width}x{self.height}')
         self._label = Label(self._window)
         self._label.pack()
-        self._projection = Matrix44.perspective_projection(90, self.width / self.height, 0.1, 1000.0)
-        self.angle = 0.0
+        self.camera = Camera(glm.vec3(0, 2, 5), glm.vec3(0, 0, 0))
 
     def _swap_buffer(self, buffer: Image.Image):
         buffer = ImageTk.PhotoImage(buffer)
@@ -34,42 +35,7 @@ class Display:
         self._label.image = buffer
 
     def _update(self):
-        bitmap = Image.new('RGBA', (self.width, self.height), color='black')
-        draw = ImageDraw.Draw(bitmap)
-        fill = (255, 255, 255, 255)
-        width = 0
-        for mesh in self.meshes:
-            translate = Matrix44.from_translation(mesh.position)
-            # rotation = Matrix44.from_z_rotation(np.deg2rad(self.angle)) * Matrix44.from_x_rotation(np.deg2rad(self.angle))
-            for v0, v1, v2 in mesh.triangles:
-                v0 = self._projection * translate * v0
-                v1 = self._projection * translate * v1
-                v2 = self._projection * translate * v2
-
-                # first step
-
-                v0.x += 1.0
-                v0.y += 1.0
-                v1.x += 1.0
-                v1.y += 1.0
-                v2.x += 1.0
-                v2.y += 1.0
-
-                # next step
-
-                v0.x *= 0.5 * float(self.width)
-                v0.y *= 0.5 * float(self.height)
-                v1.x *= 0.5 * float(self.width)
-                v1.y *= 0.5 * float(self.height)
-                v2.x *= 0.5 * float(self.width)
-                v2.y *= 0.5 * float(self.height)
-
-                draw.line([*v0.xy, *v1.xy], fill=fill, width=width)
-                draw.line([*v1.xy, *v2.xy], fill=fill, width=width)
-                draw.line([*v2.xy, *v0.xy], fill=fill, width=width)
-
-        self.angle += 1
-
+        bitmap = pipeline(self.camera, self.meshes, Light(glm.vec3(1.2, 1.0, 5.0)), self.width, self.height)
         self._swap_buffer(bitmap)
         self._window.after(1, self._update)
 
@@ -78,16 +44,7 @@ class Display:
         self._window.mainloop()
 
 
-# triangle = Mesh('triangle',
-#                 [
-#                     Vector3([0.0, 1.0, 0.0]),
-#                     Vector3([1.0, 0.0, 0.0]),
-#                     Vector3([-0.5, 0.5, 0.0]),
-#                 ],
-#                 [0, 1, 2],
-#                 position=Vector3([0., 0., 3.]))
-african_head = Mesh.load_from_obj_file('./african_head.obj', 'head')
-african_head.position = Vector3([0, 0, 3])
+obj = Mesh.from_obj_file('./head.obj')
 window = Display(WIDTH, HEIGHT, 'Software 3D render')
-window.meshes.extend([african_head])
+window.meshes.extend([obj])
 window.run()
