6fbeeda619d2aceff6c9474ca3a89661a612d030
Dmitry Yushko <yushko07@gmail.com>
2019-09-19 11:13:47 +0300

    Small code refactor in pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw
 
 from mesh import Mesh, Face
 
+
 Point2D = Tuple[int, int]
 
 
@@ -30,21 +31,23 @@ class Light:
 
 
 def get_model_matrix(mesh: Mesh) -> glm.mat4:
-    # rotation_x = glm.mat4.from_x_rotation(mesh.rotation.x)
-    # rotation_y = glm.mat4.from_y_rotation(mesh.rotation.y)
-    # rotation_z = glm.mat4.from_z_rotation(mesh.rotation.z)
-    # scale_matrix = glm.mat4.from_scale(mesh.scale)
-    # rotation_matrix = rotation_x * rotation_y * rotation_z
+    # TODO adding rotation!
     scale_matrix = glm.scale(glm.mat4(), mesh.scale)
     translate_matrix = glm.translate(glm.mat4(), mesh.position) * scale_matrix
     return translate_matrix
 
 
-def world_to_screen(vertex: glm.vec4, width: float, height: float) -> glm.vec2:
+def true_round(i: float) -> int:
+    if i % 1 >= 0.5:
+        i += 0.5
+    return int(i)
+
+
+def world_to_screen(vertex: glm.vec4, width: float, height: float) -> Point2D:
     ndc_space_pos = vertex.xyz / vertex.w
     x = ndc_space_pos.x * width + width / 2
     y = -ndc_space_pos.y * height + height / 2
-    return glm.vec2(x, y)
+    return true_round(x), true_round(y)
 
 
 def is_clipped(vec: glm.vec4) -> bool:
@@ -55,22 +58,24 @@ def is_clipped(vec: glm.vec4) -> bool:
 
 
 def draw_face(bitmap: Image.Image, face: Face, mvp: glm.mat4, width: float, height: float, fill: tuple) -> NoReturn:
+    # TODO adding culling invisible vertex
     draw = ImageDraw.Draw(bitmap)
     p0: glm.vec4 = mvp * glm.vec4(face.p0, 1.0)
     p1: glm.vec4 = mvp * glm.vec4(face.p1, 1.0)
     p2: glm.vec4 = mvp * glm.vec4(face.p2, 1.0)
 
-    p0: glm.vec2 = world_to_screen(p0, width, height)
-    p1: glm.vec2 = world_to_screen(p1, width, height)
-    p2: glm.vec2 = world_to_screen(p2, width, height)
+    p0: Point2D = world_to_screen(p0, width, height)
+    p1: Point2D = world_to_screen(p1, width, height)
+    p2: Point2D = world_to_screen(p2, width, height)
 
-    draw.line([*p0.xy, *p1.xy], fill=fill)
-    draw.line([*p1.xy, *p2.xy], fill=fill)
-    draw.line([*p2.xy, *p0.xy], fill=fill)
-    draw.polygon([*p0.xy, *p1.xy, *p2.xy], fill=fill)
+    draw.line((p0, p1), fill=fill)
+    draw.line((p1, p2), fill=fill)
+    draw.line((p2, p0), fill=fill)
+    draw.polygon((p0, p1, p2), fill=fill)
 
 
 def pipeline(camera: Camera, meshes: Sequence[Mesh], light: Light, width: int, height: int) -> Image.Image:
+    # TODO adding true backface culling and Z-buffer!
     bitmap = Image.new('RGBA', (width, height), color='black')
     for mesh in meshes:
         projection_matrix = glm.perspective(glm.radians(90.0), width/height, 0.1, 1000.0)
@@ -79,9 +84,9 @@ def pipeline(camera: Camera, meshes: Sequence[Mesh], light: Light, width: int, h
         mvp = projection_matrix * view_matrix * model_matrix
         for face in mesh.faces:
             dot = glm.dot(face.p0 - camera.position, face.normal)
-            diffuse = light.get_diffuse_color(face, model_matrix)
             if dot >= 0:
                 continue
+            diffuse = light.get_diffuse_color(face, model_matrix)
             ambient_strength = 0.1
             ambient = ambient_strength * light.color
             result = glm.vec4((ambient + diffuse) * mesh.color, 1.0)
